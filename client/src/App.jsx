ca6ed5dcf5883d55352941c0327859242c7a8562
Keegan Colaco <keeganpaulcolaco@gmail.com>
2021-11-27 17:21:50 +0530

    Frontend Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="divide-y divide-teal-400 md:divide-pink-400">
+      <div className="py-2">1</div>
+      <div className="py-2">2</div>
+      <div className="py-2">3</div>
+    </div>
+  );
+}
+
+export default App;
