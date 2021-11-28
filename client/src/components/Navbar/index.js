764e935254be3e26ff2f89b94bb4c8390f8b5bd4
Keegan Colaco <keeganpaulcolaco@gmail.com>
2021-11-28 17:55:29 +0530

    Added navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -45,16 +45,71 @@ function MobileNav() {
     )
 }
 
-// function LargeNav() {
-//     return ()
-// }
+function LargeNav() {
+    const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+    const [user, setUser] = useState({
+        fullName: "keegan"
+    });
+    return (
+        <>
+            <div className="hidden lg:inline container px-20 mx-auto">
+                <div className="gap-4 w-full items-center justify-around flex">
+                    <div className="w-28">
+                        <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="" className="w-full h-full" />
+                    </div>
+                    <div className="w-3/4 bg-white shadow-md p-3 flex items-center gap-3 border border-gray-200 rounded">
+                        <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
+                            <span className="text-zomato-400">
+                                <HiLocationMarker />
+                            </span>
+                            <input type="text" placeholder="Delhi NCR" className="focus:outline-none" />
+                            <IoMdArrowDropdown />
+                        </div>
+                        <div className="flex w-full items-center gap-2">
+                            <RiSearch2Line />
+                            <input type="search" placeholder="Search for restaurant, cuisine or a dish" className="w-full focus:outline-none" />
+                        </div>
+                    </div>
+                    {user?.fullName ? (
+                        <div className="relative w-20">
+                            <div
+                                onClick={() => setIsDropDownOpen((prev) => !prev)}
+                                className="border border-gray-300 text-zomato-400 w-full h-20 rounded-full"
+                            >
+                                <img
+                                    src="https://cdn1.vectorstock.com/i/1000x1000/36/15/businessman-character-avatar-isolated-vector-12613615.jpg"
+                                    alt=""
+                                    className="w-full h-full rounded-full object-cover"
+                                />
+                            </div>
+                            {isDropDownOpen && (
+                                <div className="absolute shadow-lg py-3 -bottom-20 -right-4 w-full bg-white z-20 flex flex-col gap-2">
+                                    <button>Sign Out</button>
+                                </div>
+                            )}
+                        </div>
+                    ) : (
+                        <div className=" flex gap-4">
+                            <button className="text-gray-500 text-xl hover:text-gray-800">
+                                Login
+                            </button>
+                            <button className="text-gray-500 text-xl hover:text-gray-800">
+                                Signup
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </>
+    )
+}
 
 function Navbar() {
     return (
         <>
             <nav className="p-4 flex bg-white shadow-md lg:shadow-none w-full items-center">
                 <MobileNav />
-                {/* <LargeNav /> */}
+                <LargeNav />
             </nav>
         </>
     )
